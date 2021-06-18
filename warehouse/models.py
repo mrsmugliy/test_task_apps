2baea16b7d7cf536dee984586e541ec9b3209657
mrsmugliy <mr,smugliy@gmail.com>
2021-06-18 13:48:05 +0200

    get merge dev_warehouse into master

---
+++ warehouse/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+# Create your models here.
+class Warehouse(models.Model):
+    """
+    Warehouse model attributes
+    """
+    name = models.CharField(max_length=64, verbose_name='Warehouse name')
+    is_active = models.BooleanField(default=False)
+    address1 = models.CharField(max_length=255, verbose_name='First address')
+    address2 = models.CharField(max_length=255, verbose_name='Second address')
+    city = models.CharField(max_length=64)
+    country = models.CharField(max_length=64)
+    zip = models.CharField(max_length=8)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Date of creation')
+    modified_at = models.DateTimeField(auto_now=True, verbose_name='Date of change')
+
+    def __str__(self):
+        return f'id: {self.id}'
+
+
+class WarehouseProduct(models.Model):
+    """
+    Warehouse product model attributes
+    """
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+    sku = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Date of creation')
+    modified_at = models.DateTimeField(auto_now=True, verbose_name='Date of change')
